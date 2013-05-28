d59380ffc804cdaf186bc174afe6a1a551fc1827
soutennizavan@gmail.com <vnguyen.engineyard.com>
2013-05-28 12:48:07 -0400

    initiral

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0515c0bb936300d3466e9ade8a61ab3563a40b77571b84c6b91157eec408094035c28665f203508aada5117362451ae7c46741ea1967b0acc99f7a2291020f20'
